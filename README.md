4def9fb94dbb0cb68d7113fd921cfd0a31a15884
Axel Jalonen <axeljalonen@gmail.com>
2025-01-31 17:47:59 -0800

    Fix README typo

--- README.md
+++ README.md
@@ -2,12 +2,19 @@
 
 ## Usage
 
-1. (Optionally) Edit `num_threads` in `src/main.rs`
-2. Edit `prefix` in `src/main.rs` (Must be valid base58, (the program assumes so)).
-3. Build & run in release mode
+1. Configure options:
+  - (Automatically binary encoded base58 string) Prefix
+    - Your chosen prefix
+  - (boolean) All capitals
+    - Creates a vector of all legal base58 strings in capital variation from your prefix.
+  - (boolean) Substring
+    - Searches for substrings, not just prefixs.
+  - (Natural number) Edit `num_threads` in `src/main.rs`
+    - Number of threads to search with, usually use as many threads as you have cores.
+4. Build & run in release mode
 
 ## Building for linux from MacOS:
 
-Install zibuild, etc. 
+Install zigbuild, etc. 
 
 ```cargo zigbuild --target x86_64-unknown-linux-musl --release```
--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,6 @@ fn get_public_key(bits: &mut [u8; 32]) -> [u8; 32] {
     let compressed = point.compress().as_bytes().clone();
 
     compressed
-    // PublicKey(compressed, point)
 }
 
 fn derive_public_key(private_key_bytes: [u8; 32]) -> [u8; 32] {
@@ -49,14 +48,15 @@ fn derive_public_key(private_key_bytes: [u8; 32]) -> [u8; 32] {
     // .expect("Failed to create secret key from private key bytes");
     // let public_key: PublicKey = PublicKey::from(&secret_key);
     // public_key.to_bytes()
+
     let mut h: Sha512 = Sha512::new();
     let mut hash: [u8; 64] = [0u8; 64];
     let mut digest: [u8; 32] = [0u8; 32];
 
     h.update(private_key_bytes);
     hash.copy_from_slice(h.finalize().as_slice());
-
     digest.copy_from_slice(&hash[..32]);
+
     let public_key = get_public_key(&mut digest);
     public_key
 }
@@ -76,16 +76,67 @@ fn make_key_pair(public_key: [u8; 32], private_key: [u8; 32]) -> [u8; 64] {
     key_pair
 }
 
+fn generate_capital_variations(input: &str) -> Vec<Vec<u8>> {
+    let base58_invalid_chars = ['0', 'O', 'I', 'l'];
+    let chars: Vec<char> = input.chars().collect();
+    let mut results = Vec::new();
+    let len = chars.len();
+    let mut variations = vec![Vec::new()];
+
+    for i in 0..len {
+        let mut new_variations = Vec::new();
+        let c = chars[i];
+
+        if c.is_ascii_alphabetic() && !base58_invalid_chars.contains(&c) {
+            for v in &variations {
+                let mut lower = v.clone();
+                lower.push(c.to_ascii_lowercase() as u8);
+                new_variations.push(lower);
+
+                let mut upper = v.clone();
+                upper.push(c.to_ascii_uppercase() as u8);
+                new_variations.push(upper);
+            }
+        } else {
+            for v in &variations {
+                let mut unchanged = v.clone();
+                unchanged.push(c as u8);
+                new_variations.push(unchanged);
+            }
+        }
+
+        variations = new_variations;
+    }
+
+    results.extend(variations);
+    results
+}
+
 fn main() {
-    let prefix = b"9WzDXwBbmkg8ZTbNMqUxvQRAyrZzDsGYdLVL9zYtAWWM";
-    let prefix_alt = b"9WzDXwBbmkg8ZTbNMqUxvQRAyrZzDsGYdLVL9zYtAWWM";
-    let prefix_alt_two = b"9WzDXwBbmkg8ZTbNMqUxvQRAyrZzDsGYdLVL9zYtAWWM";
+    // START CONFIGURATION
+    let prefix_str = "prefix";
+    // Enable all combinations of capitals
+    let enable_all_caps_alt = true;
+    // Enable searching for substrings at arbitrary position
+    let enable_search_for_substring = false;
     let num_threads = 8;
-    let prefix_clone = prefix.to_vec();
+    // END CONFIGURATION
+
+    let mut prefixs: Vec<Vec<u8>> = vec![prefix_str.as_bytes().to_vec()];
+
+    if enable_all_caps_alt {
+        prefixs = generate_capital_variations(prefix_str);
+    }
+
+    let prefix_space = &prefixs.len();
+    let prefix_len = prefix_str.len();
+    let prefix_clone = prefixs.to_vec();
     let mut handles = vec![];
 
     for _ in 0..num_threads {
-        let prefix_clone = prefix_clone.clone();
+        let prefix_len = prefix_len.clone();
+        let prefix_space = prefix_space.clone() as u64;
+        let prefix_clone_two = prefix_clone.clone();
 
         let handle = thread::spawn(move || {
             let mut counter = 0;
@@ -107,23 +158,47 @@ fn main() {
 
                 if counter % 100_000 == 0 {
                     let elapsed = start_time.elapsed().as_secs();
+                    let raw_key_count = counter * num_threads;
+                    let raw_key_rate = counter / elapsed * num_threads;
+                    let mut effective_key_count = raw_key_count * prefix_space;
+                    let mut effective_key_rate = raw_key_rate * prefix_space;
+                    if enable_search_for_substring {
+                        let factor: u64 = 32u64 - prefix_len as u64;
+                        effective_key_count *= factor;
+                        effective_key_rate *= factor;
+                    }
                     print!(
-                        "\rChecked {} keys. Generating approx {} keys/sec",
-                        counter * num_threads,
-                        counter / elapsed * num_threads
+                        "\rChecked {} keys. Generating approx {} keys/sec, EFFECTIVE: {}, {}",
+                        raw_key_count, raw_key_rate, effective_key_count, effective_key_rate
                     );
                     io::stdout().flush().unwrap();
                 }
 
-                if encoded.starts_with(&prefix_clone)
-                    || encoded.starts_with(&prefix_alt.clone())
-                    || encoded.starts_with(&prefix_alt_two.clone())
-                {
-                    println!(
-                        "\x1b[32m Found key pair: {:?}\x1b[0m",
-                        make_key_pair(public_key, private_key)
-                    );
-                    println!("Public key: {:?}", bs58::encode(public_key).into_string());
+                fn contains_subsequence_xor(encoded: &[u8], prefix: &[u8]) -> bool {
+                    encoded.windows(prefix.len()).any(|window| {
+                        window
+                            .iter()
+                            .zip(prefix.iter())
+                            .all(|(&a, &b)| (a ^ b) == 0)
+                    })
+                }
+
+                for prefix in &prefix_clone_two {
+                    let condition: bool;
+
+                    if enable_search_for_substring {
+                        condition = contains_subsequence_xor(&encoded, &prefix)
+                    } else {
+                        condition = encoded.starts_with(prefix);
+                    }
+
+                    if condition {
+                        println!(
+                            "\x1b[32m Found key pair: {:?}\x1b[0m",
+                            make_key_pair(public_key, private_key)
+                        );
+                        println!("Public key: {:?}", bs58::encode(public_key).into_string());
+                    }
                 }
             }
         });
